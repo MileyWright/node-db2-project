02774fcd831de9b6987e117a318822ddb598c389
Miley Wright <dishaliekwright@yahoo.com>
2020-01-31 01:04:49 -0500

    fixed bug

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('cars').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('cars').insert([
         {id: 1, vin: 'KMHGN41EMFU058646', make: 'Hyundai', model: 'Genesis', mileage: '5481', transmission_type: 'Automatic', title_status: ''},
         {id: 2, vin: '1FA6P8NF4K5144678', make: 'Ford', model: 'Mustang', mileage: '3102', transmission_type: 'Automatic', title_status: ''},
         {id: 3, vin: 'JM0GL103300400730', make: 'Mazda', model: '6 Touring GL', mileage: '2007', transmission_type: 'Automatic', title_status: ''},
