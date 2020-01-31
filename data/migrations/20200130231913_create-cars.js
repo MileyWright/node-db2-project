c1399ef9104cc83ea9f4488b69e344d7cd6fff82
Miley Wright <dishaliekwright@yahoo.com>
2020-01-31 00:40:22 -0500

    completed the migration

--- data/migrations/20200130231913_create-cars.js
+++ data/migrations/20200130231913_create-cars.js
@@ -1,8 +1,28 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('cars', cars => {
+      // id
+      cars.increments();
+      //vin number
+      cars.integer('VIN')
+      .notNullable()
+      .unique();
+      // the make
+      cars.string('make', 128)
+      .notNullable();
+      // the model
+      cars.string('model', 128)
+      .notNullable();
+      // mileage
+      cars.decimal('mileage')
+      .notNullable();
+      //transmission type
+      cars.string('transmission_type', 128);
+      // title status
+      cars.string('title_status', 128);
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('cars');
 };
