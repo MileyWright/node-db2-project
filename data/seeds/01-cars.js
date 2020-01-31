e2f39147ad36ddfa0659f4bf360c16a899e4ddc3
Miley Wright <dishaliekwright@yahoo.com>
2020-01-31 01:03:33 -0500

    created seeds

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+        {id: 1, vin: 'KMHGN41EMFU058646', make: 'Hyundai', model: 'Genesis', mileage: '5481', transmission_type: 'Automatic', title_status: ''},
+        {id: 2, vin: '1FA6P8NF4K5144678', make: 'Ford', model: 'Mustang', mileage: '3102', transmission_type: 'Automatic', title_status: ''},
+        {id: 3, vin: 'JM0GL103300400730', make: 'Mazda', model: '6 Touring GL', mileage: '2007', transmission_type: 'Automatic', title_status: ''},
       ]);
     });
 };
