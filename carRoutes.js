da0c84f8c66a8245bb4c23aad0a6e81a144d3fc5
Miley Wright <dishaliekwright@yahoo.com>
2020-01-31 22:28:55 -0500

    POST request /cars

--- carRoutes.js
+++ carRoutes.js
@@ -15,7 +15,7 @@ router.get('/', (req, res) => {
 })
 
 // GET request /cars/:id
-router.get('/:id', validateAccountID, (req, res) => {
+router.get('/:id', validateCarID, (req, res) => {
     const id = req.params.id;
     db('cars')
     .where({'id': id})
@@ -27,8 +27,37 @@ router.get('/:id', validateAccountID, (req, res) => {
     })
 })
 
+// POST request /cars
+router.post('/', (req, res) => {
+    const data = {
+        VIN: req.body.VIN,
+        make: req.body.make,
+        model: req.body.model,
+        mileage: req.body.mileage,
+        transmission_type: req.body.transmission_type,
+        title_status: req.body.title_status,
+    };
+    db('cars')
+        .insert(data)
+        .then(ids => {
+            const id = ids[0];
+            db('cars')
+                .select('VIN', 'make', 'model', 'mileage', 'transmission_type', 'title_status')
+                .where({id})
+                .first()
+                .then(posts => {
+                    res.status(200).json(posts)
+                })
+                
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({error: 'Could not post account info'})
+        })
+})
+
 //custom middleware
-function validateAccountID(req, res, next) {
+function validateCarID(req, res, next) {
     const id = req.params.id;
     db('cars')
         .where({'id': id})
@@ -41,7 +70,7 @@ function validateAccountID(req, res, next) {
         })
 }
 
-function validateAccount(req, res, next) {
+function validateCar(req, res, next) {
     const data = req.body;
     if(!data.VIN || !data.make || !data.model || !data.mileage){
         res.status(400).json({message: 'missing car data'})
