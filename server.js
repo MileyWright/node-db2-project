233dff4d41621ece963407d7b9311d3cc9b3d1e4
Miley Wright <dishaliekwright@yahoo.com>
2020-01-31 21:21:23 -0500

    server is up

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const server = express();
+
+const cars = require('./carRoutes')
+
+server.use(express.json());
+
+server.use('/cars', cars);
+
+server.get('/', (req, res) => {
+    res.send(
+       <h2>Welcome to my API</h2>
+    )
+})
