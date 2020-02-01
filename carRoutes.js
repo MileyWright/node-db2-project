d18dc13ced5ac827b346c247ea5849069f408256
Miley Wright <dishaliekwright@yahoo.com>
2020-01-31 21:50:53 -0500

    GET request /cars

--- carRoutes.js
+++ carRoutes.js
@@ -2,4 +2,16 @@ const express = require('express');
 const db = require('./data/dbConfig.js');
 const router = express.Router();
 
+//GET request /cars
+router.get('/', (req, res) => {
+    db('cars')
+    .then(car => {
+        res.status(200).json(car)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({error: 'Cannot retrieve cars'})
+    })
+})
+
 module.exports = router;
